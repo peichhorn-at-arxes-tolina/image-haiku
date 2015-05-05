de35fdd0cc66677b2c5e08d29a64f64a67c01d75
Philipp Eichhorn <peichhor@web.de>
2015-05-05 21:54:06 +0200

    Step 2: Basic Webapp

--- src/main/java/de/fips/imagehaiku/ImageHaikuApplication.java
+++ src/main/java/de/fips/imagehaiku/ImageHaikuApplication.java
@@ -1,8 +1,31 @@
 package de.fips.imagehaiku;
 
-public class ImageHaikuApplication {
+import de.fips.imagehaiku.configuration.ImageHaikuConfiguration;
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 
-    public static void main(String[] args) {
-        System.out.println("Image Haiku. Done!");
+public class ImageHaikuApplication extends Application<ImageHaikuConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        if (args == null || args.length != 2) {
+            args = new String[]{"server", "./server/config.yml"};
+        }
+        new ImageHaikuApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "Image Haiku";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<ImageHaikuConfiguration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/assets", "/", "index.html"));
+    }
+
+    @Override
+    public void run(final ImageHaikuConfiguration configuration, final Environment environment) throws Exception {
     }
 }
---
+++ src/main/java/de/fips/imagehaiku/configuration/ImageHaikuConfiguration.java
@@ -0,0 +1,6 @@
+package de.fips.imagehaiku.configuration;
+
+import io.dropwizard.Configuration;
+
+public class ImageHaikuConfiguration extends Configuration {
+}
