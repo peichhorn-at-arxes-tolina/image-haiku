ef6be6c3120038a8cf2937d64111a195f730a043
Philipp Eichhorn <peichhor@web.de>
2015-05-05 21:55:33 +0200

    Step 3: Changes to the static resources should not require a server restart in development.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        sync: {
+            main: {
+                files: [{
+                    cwd: 'src/main/resources/assets',
+                    src: ['**'],
+                    dest: 'build/resources/main/assets'
+                }]
+            }
+        },
+        watch: {
+           files: 'src/main/resources/assets/**',
+           tasks: ['sync']
+       }
+    });
+
+    grunt.loadNpmTasks('grunt-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', 'sync', 'watch');
+};
